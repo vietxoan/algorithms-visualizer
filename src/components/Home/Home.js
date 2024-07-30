83f85e2f865d17ad4ff38fcc31e8b53c724d9f71
viet nguyen <nqvn1998@gmail.com>
2024-07-30 10:49:27 +0700

    create main page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import '../Home/Home.css'
+
+// e900f0d8
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e900f0d8';
+function Home() {
+  return (
+    <h1>
+      Please choose algorithm
+    </h1>
+  )
+}
+
+export default Home
